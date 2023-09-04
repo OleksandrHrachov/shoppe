4243b1ed9cee564a3c0f1fdec0491d07fec329e9
o.hrachov <ol.grachov@gmail.com>
2023-09-04 20:37:35 +0300

    changed to HashRouter

--- shoppe/src/main.tsx
+++ shoppe/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App'
 import './index.scss'
 import { store } from "./store";
@@ -9,9 +9,9 @@ import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
